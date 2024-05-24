f0b21a567c2fcd9360f2be92e0c0a498fbdbf4be
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-05-24 09:49:07 +0200

    make the pizza still to be finished

---
+++ public/scripts/app.js
@@ -0,0 +1,40 @@
+
+let ingredients1 = "100 gr flour" + "1 tablespoon salt" + "1 teaspoon sugar" + "65 ml of warm water" + "Half a tablespoon of extra virgin olive oil" + "Half an envelope of yeast";
+
+let ingredients2 = "Tomato sauce" + "Mozzarella cheese" + "Salami, Italian pepperoni" + "Black pepper, oregano and basil" + "A little extra virgin olive oil";
+
+let step1 = "Lets start with the dough! We put together the flour";
+
+let step2 = "Lets start with the dough! We put together the salt";
+
+let step3 = "Lets start with the dough! We put together the sugar";
+
+let step4 = "Lets start with the dough! We put together the flour";
+
+let step5 = "Lets start with the dough! We put together the olive oil in a mixing bowl";
+
+let step6 = "Then, we dissolve the yeast in the warm water and add it to the other ingredients";
+
+let step7 = "We mix everything well until it is a ball";
+
+let step8 = "Then cover it with a cloth and leave it for about an hour";
+
+let step9 = "Then cover it with a cloth and leave it for about an hour";
+
+let step10 = "After the resting time, we extend the dough in a round or square shape";
+
+let step11 = "The best, if we want an authentic Neapolitan pizza, will be to make it round. We can add a little flour on the surface and on the rolling pin";
+
+let step12 = "When the dough is ready we add the ingredients to make our pepperoni pizza. First we put the tomato sauce together with the spices";
+
+let step13 = "Then we place the salami and finally the mozzarella cheese";
+
+let step14 = "If we like cheese we can also add grated cheese on top of the mozzarella";
+
+let step15 = "We put it in the oven for half an hour at 170ºC, although the time is relative! You have to pay attention to the edges of the pizza and the cheese, and when the edges are golden brown and the cheese is well melted, the pizza is ready"
+
+function () {
+    
+}
+ 
+console.log();
