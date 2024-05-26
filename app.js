2a74483c5dfcbaf39a4c2c7906c7adcadbb82d38
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-05-26 12:05:01 +0200

    pizza exercise level 1 app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-import  promptSync from "prompt-sync";
-import { makePizza } from "./public/scripts/makePizza";
-import { deliverPizza } from "./public/scripts/deliverPizza";
+const ingredientsMass = ["100 gr flour" + "1 tablespoon salt" + "1 teaspoon sugar" + "65 ml of warm water" + "Half a tablespoon of extra virgin olive oil" + "Half an envelope of yeast"];
 
-function app() {
-    const prompt = promptSync();
-    const pizzaName = prompt("And your pizza is ... (pepperoni): ");
+const ingredientsClass = ["Tomato sauce" + "Mozzarella cheese" + "Salami, Italian pepperoni" + "Black pepper, oregano and basil" + "A little extra virgin olive oil"];
 
-    let result = makePizza (pizzaName);
+let preparationPizza = ["Let's start with the dough! Gather the flour, salt, sugar and olive oil in a bowl to whisk.", "Then, dissolve the yeast in the warm water and add it to the other ingredients. Mix everything well until it is a ball.", "Then cover it with a cloth and leave it for about an hour to grow.", "After the resting time, roll out the dough into a round or square shape. The best, if we want an authentic Neapolitan pizza, will be to make it round. We can add a little flour on the surface and on the rolling pin.", "When the dough is ready we add the ingredients to make our pepperoni pizza.", "First add the tomato sauce together with the spices.", "Then we place the salami", "Then we put mozzarella cheese", "We put it in the oven for half an hour at 170ºC, although the time is relative! Pay attention to the edges of the pizza and the cheese, and when the edges are golden brown and the cheese is well melted, the pizza is ready!", "Pizza delivered"];
 
-    result.then((resolve) => {
-        if (resolve === undefined) deliverPizza();
-    });  
-}
+const pizza = "peperoni";
 
-app();
+preparationPizza.forEach(element => {
+    console.log(element);
+})
