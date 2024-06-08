536edf31e5d5d6527a1169db3afbcfe8ae84760e
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-08 13:01:27 +0200

    completed level one of the requested exercises and completed the readme.md

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
 
-#npm
-##installation
+# npm
+## installation
 ````
 npm install
 ````
+
+# JS - Pizza Theory
+
+## Level 1:
+Elaboration of a pizza with javascript using variables, functions and “console.log()” from a single file (app.js) without modulation.
+Expected terminal output: steps followed for the elaboration until the delivery to the customer.
+Example of terminal output (it is only an example you don't have to have the same steps):
+Output:
+mix 30 grams of fresh yeast with 2½ cups of warm water.
+add 2 tablespoons of olive oil
+add flour
+add 1 teaspoon of fine salt
+knead well until it is no longer sticky
+Shape the dough for 4 pizzas.
+4 portions obtained
+base for 1 pizza
+Let the base for 1 pizza rest for 45 min.
+Obtained base with circular shape
+added Tomato sauce
+added Mozzarella
+added Pepperoni
+added 4 Black Olives
+Baking the pizza for 10 min
+Pizza ready
+Pizza Delivered
+
+## Level 2:
+The software will now allow interaction with the customer via terminal. 
+
+(using the node prompt-sync package).  .
+The customer will order the pizza by typing the name of the pizza into the terminal.
+The code should be modular
+Example of terminal output :
+Input:And your pizza is ... (pepperoni): pepperoni
+
+Output:
+Pizza pepperoni in preparation
+Picking up the dough
+Stretching the dough
+Adding tomato
+Adding mozzarella
+Adding pepperoni
+Baking the pizza
+Pizza ready
+Pizza delivered
+
+## Extra Challenge:
+The software should allow the preparation of any pizza that is added to the list of available pizzas and if it is not available, the software should allow the preparation of any pizza that is added to the list of available pizzas.
--- app.js
+++ app.js
@@ -1,11 +1,57 @@
-const ingredientsMass = ["100 gr flour" + "1 tablespoon salt" + "1 teaspoon sugar" + "65 ml of warm water" + "Half a tablespoon of extra virgin olive oil" + "Half an envelope of yeast"];
+function mezclarLevadura() {
+    console.log("Mezclar 30 gramos de levadura fresca con 2½ tazas de agua tibia");
+}
 
-const ingredientsClass = ["Tomato sauce" + "Mozzarella cheese" + "Salami, Italian pepperoni" + "Black pepper, oregano and basil" + "A little extra virgin olive oil"];
+function añadirAceite() {
+    console.log("Añadir 2 cucharadas soperas de aceite de oliva");
+}
 
-let preparationPizza = ["Let's start with the dough! Gather the flour, salt, sugar and olive oil in a bowl to whisk.", "Then, dissolve the yeast in the warm water and add it to the other ingredients. Mix everything well until it is a ball.", "Then cover it with a cloth and leave it for about an hour to grow.", "After the resting time, roll out the dough into a round or square shape. The best, if we want an authentic Neapolitan pizza, will be to make it round. We can add a little flour on the surface and on the rolling pin.", "When the dough is ready we add the ingredients to make our pepperoni pizza.", "First add the tomato sauce together with the spices.", "Then we place the salami", "Then we put mozzarella cheese", "We put it in the oven for half an hour at 170ºC, although the time is relative! Pay attention to the edges of the pizza and the cheese, and when the edges are golden brown and the cheese is well melted, the pizza is ready!", "Pizza delivered"];
+function añadirHarinaYSal() {
+    console.log("Añadir Harina de fuerza");
+    console.log("Añadir 1 cucharadita de sal fina");
+}
 
-const pizza = "peperoni";
+function amasar() {
+    console.log("Amasar bien hasta que deje de ser pegajosa");
+}
 
-preparationPizza.forEach(element => {
-    console.log(element);
-})
+function darFormaMasa() {
+    console.log("Darle forma a la masa para 4 pizzas");
+    console.log("4 porciones obtenidas");
+}
+
+function prepararBasePizza() {
+    console.log("Base para 1 pizza");
+    console.log("Dejar la base para 1 pizza reposar durante 45 min");
+    console.log("Base con forma circular obtenida");
+}
+
+function añadirIngredientes() {
+    console.log("Añadido Salsa de tomate");
+    console.log("Añadido Mozzarella");
+    console.log("Añadido Pepperoni");
+    console.log("Añadidas 4 Aceitunas Negras");
+}
+
+function hornearPizza() {
+    console.log("Horneando la pizza durante 10 min");
+}
+
+function pizzaLista() {
+    console.log("Pizza lista");
+}
+
+function entregarPizza() {
+    console.log("Pizza entregada");
+}
+
+mezclarLevadura();
+añadirAceite();
+añadirHarinaYSal();
+amasar();
+darFormaMasa();
+prepararBasePizza();
+añadirIngredientes();
+hornearPizza();
+pizzaLista();
+entregarPizza();
